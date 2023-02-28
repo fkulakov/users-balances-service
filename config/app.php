fef3cedeb1da1cf7fe66711d445ff75ee7aaed33
Fyodor Kulakov <fkulakov@gmail.com>
2023-02-28 12:53:21 +0400

    fix: remove unnecessary HorizonServiceProvider
    Signed-off-by: Fyodor Kulakov <fkulakov@gmail.com>

--- config/app.php
+++ config/app.php
@@ -192,7 +192,6 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
 
     ],
 
